653b3db6ff18865610d4ef7e80bede706bd7abcf
Tomasz Pupiec <tomasz.pupiec@netrix.com.pl>
2018-09-17 14:00:57 +0200

    Test commit 2

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import * as lineIntersection from 'line-intersection';
 
-const intersect = lineIntersection.findIntersection([{ x:1, y: 1 }, { x: 0, y: 0 }, { x: -1, y: 1 }, { x: 1, y: -1 }]);
+const points = [{ x:1, y: 1 }, { x: 0, y: 0 }, { x: -1, y: 1 }, { x: 1, y: -1 }];
 
-console.log(intersect);
+console.log(lineIntersection.findIntersection(points));
+
+console.log(lineIntersection.isSegmentIntersected(points));
+
