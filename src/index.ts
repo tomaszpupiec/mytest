b7189c96be3535c3bca577d435409b15891e5f4d
Tomasz Pupiec <tomasz.pupiec@netrix.com.pl>
2018-09-19 12:08:11 +0200

    Testing: Delete comments

--- src/index.ts
+++ src/index.ts
@@ -8,20 +8,3 @@ console.log(lineIntersection.findIntersection(points));
 // comment 2
 console.log(lineIntersection.isSegmentIntersected(points));
 
-// comment 3
-
-// comment 4
-
-// comment 5
-
-// comment 6
-
-// comment 7
-
-// comment 8
-
-// comment 9
-
-// comment 10
-
-// comment 11
